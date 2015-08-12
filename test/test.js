f40695e39ce461f3e1320b60b440cce56d439543
Artem Sapegin <artem@sapegin.ru>
2015-08-12 07:49:54 +0200

    Test source code, not dist.

--- test/test.js
+++ test/test.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var should = require('should'),
-	Block = require('../dist/bem-cn');
+	Block = require('../src/bem-cn');
 
 describe('Wrapper function Block', function() {
 	it('should be a function', function(){
