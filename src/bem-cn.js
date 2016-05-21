8a52f0c2a776e2d730c648b1d93b2ab9ac960c25
Alexander Burtsev <albburtsev@gmail.com>
2016-05-21 13:30:55 +0300

    JSDocs for selector generator

--- src/bem-cn.js
+++ src/bem-cn.js
@@ -1,8 +1,3 @@
-import {trim} from './helpers';
-
-export const ERROR_BLOCK_NAME_TYPE = 'Block name should be a string';
-export const ERROR_BLOCK_NAME_EMPTY = 'Block name should be non-empty';
-
 /**
  * How it's working?
  * The essential part of this module is based on using currying pattern.
@@ -29,6 +24,23 @@ const block = (settings) => {
 	return selector(settings);
 };
  */
+import {trim} from './helpers';
+
+export const ERROR_BLOCK_NAME_TYPE = 'Block name should be a string';
+export const ERROR_BLOCK_NAME_EMPTY = 'Block name should be non-empty';
+
+/**
+ * Selector generator, self-curried function
+ * @param {Object} settings
+ * @param {String} [settings.ns = ''] Namespace for all classes
+ * @param {String} [settings.el = '__'] Delimiter before element name
+ * @param {String} [settings.mod = '_'] Delimiter before modifier name
+ * @param {String} [settings.modValue = '_'] Delimiter before modifier value
+ * @param {Object} [settings.classMap = null]
+ * @param {Object} context
+ * @param {String} context.name Block name
+ * @return {Function}
+ */
 const selector = (settings, context) => {
 	const inner = () => {
 		return selector(settings, context);
@@ -47,7 +59,7 @@ const selector = (settings, context) => {
 	};
 
 	inner.toString = inner.valueOf = () => {
-		let {name} = settings;
+		let {name} = context;
 		return name;
 	};
 
@@ -65,12 +77,10 @@ const selector = (settings, context) => {
 /**
  * Creates new BEM block
  * @param {String} name
- * @param {Object} [settings]
+ * @todo: second parameter `settings`
  * @return {Function} Selector generator
  */
-const block = (name, settings = {}) => {
-	// @todo defaultSettings
-
+const block = (name) => {
 	if (typeof name !== 'string') {
 		throw new Error(ERROR_BLOCK_NAME_TYPE);
 	}
@@ -89,10 +99,14 @@ const block = (name, settings = {}) => {
 		// @todo
 	};
 
-	// @todo: required polyfill or helper
-	Object.assign(settings, {name});
-
-	return selector(settings);
+	// It is easy to define default settings here
+	return selector({
+		ns: '',
+		el: '__',
+		mod: '_',
+		modValue: '_',
+		classMap: null
+	}, {name});
 };
 
 export default block;
