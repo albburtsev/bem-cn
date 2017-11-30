2cbaad454029f5ca80997ce93bdba7c0f3bb8dbd
Alexander Burtsev <albburtsev@gmail.com>
2017-12-01 01:26:47 +0300

    Use TypeScript to write new version

---
+++ src/bem-cn.ts
@@ -0,0 +1,3 @@
+const message = 'Hello!'
+
+console.log(message)
--- webpack.config.js
+++ webpack.config.js
@@ -1,49 +1,56 @@
-'use strict';
+const _ = require('lodash');
+const path = require('path');
+const webpack = require('webpack');
+const project = require('./package.json');
+const banner = _.template(
+	'<%= name %> v<%= version %>\n' +
+	'<%= description %>\n' +
+	'@author <%= author.name %>, <%= author.url %>'
+)(project);
+const rootPath = path.resolve(__dirname);
 
-var _ = require('lodash'),
-    webpack = require('webpack'),
-    project = require('./package.json'),
-    banner = _.template(
-        '<%= name %> v<%= version %>\n' +
-        '<%= description %>\n' +
-        '@author <%= author.name %>, <%= author.url %>'
-    )(project);
-
-var config = {
-    module: {
-        preLoaders: [
-            {
-                test: /\.js$/,
-                loader: 'eslint'
-            }
-        ],
-        loaders: [
-            {
-                test: /\.js$/,
-                loader: 'babel',
-                exclude: /node_modules/
-            }
-        ]
-    },
-    output: {
-        library: 'block',
-        libraryTarget: 'umd'
-    },
-    resolve: {
-        extensions: ['', '.js']
-    },
-    plugins: [
-        new webpack.optimize.OccurenceOrderPlugin(),
-        new webpack.BannerPlugin(banner)
-    ]
+const config = {
+	entry: path.join(rootPath, 'src/bem-cn.ts'),
+	module: {
+		preLoaders: [
+			{
+				test: /\.js$/,
+				loader: 'eslint'
+			}
+		],
+		loaders: [
+			{
+				test: /\.js$/,
+				loader: 'babel',
+				exclude: /node_modules/
+			},
+			{
+				test: /\.tsx?$/,
+				loader: 'awesome-typescript-loader'
+			}
+		]
+	},
+	output: {
+		path: path.join(rootPath, 'dist/'),
+		filename: 'bem-cn.js',
+		library: 'block',
+		libraryTarget: 'umd'
+	},
+	resolve: {
+		extensions: ['', '.js']
+	},
+	plugins: [
+		new webpack.optimize.OccurenceOrderPlugin(),
+		new webpack.BannerPlugin(banner)
+	]
 };
 
 if (process.env.NODE_ENV === 'production') {
-    config.plugins.push(new webpack.optimize.UglifyJsPlugin({
-        compressor: {
-            warnings: false
-        }
-    }));
+	config.plugins.push(new webpack.optimize.UglifyJsPlugin({
+		compressor: {
+			warnings: false
+		}
+	}));
 }
 
 module.exports = config;
