aa85a9516369357403d3875333257392319595dd
Alexander Burtsev <burtsev@Alexanders-MacBook-Pro.local>
2018-07-23 22:58:42 +0300

    chore: repair test infrastructure

---
+++ src/index.spec.ts
@@ -0,0 +1,9 @@
+import * as should from 'should'
+import { Fake } from './index'
+
+describe('fake test', () => {
+    it('should be a function', () => {
+        const fake = new Fake()
+		should(fake).be.an.instanceOf(Fake);
+	});
+})
